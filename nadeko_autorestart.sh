203218c32b8c1fa92e34a76c7adf75ace6a5b7d7
Sunny Lin <sunnylin@synology.com>
2017-10-12 17:47:28 +0800

    Added Redis Server startup

--- nadeko_autorestart.sh
+++ nadeko_autorestart.sh
@@ -20,6 +20,10 @@ mkdir -p /root/nadeko/patch
 cd /root/nadeko/patch && bash "./patch.sh"
 
 echo ""
+echo "Starting Redis-Server"
+/usr/bin/redis-server --daemonize yes
+
+echo ""
 echo "Running NadekoBot with auto restart Please wait."
 cd /opt/NadekoBot/src/NadekoBot
 while :; do dotnet run -c Release; sleep 5s; done
