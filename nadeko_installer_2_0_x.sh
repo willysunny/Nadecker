103c2ca3212f7975f2aecb8b97ad4aa824425753
BuildTools <unconfigured@null.spigotmc.org>
2017-10-22 12:59:25 +0800

    After ImageSharp, it's SixLabors' Core

--- nadeko_installer_2_0_x.sh
+++ nadeko_installer_2_0_x.sh
@@ -43,6 +43,7 @@ echo "Download done"
 echo ""
 echo "Building NadekoBot"
 dotnet add /opt/NadekoBot/src/NadekoBot package SixLabors.ImageSharp --version 1.0.0-beta0001 -s https://www.myget.org/F/imagesharp/api/v3/index.json
+dotnet add /opt/NadekoBot/src/NadekoBot package SixLabors.Core --version 1.0.0-ci0004 -s https://www.myget.org/F/sixlabors/api/v3/index.json
 dotnet build --configuration Release
 echo ""
 echo "Building done."
